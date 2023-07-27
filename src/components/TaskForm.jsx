f679b3ca16048677936ea4b426d2c413151a02f1
Aaron Obed Girón <62667714+agirong@users.noreply.github.com>
2023-07-26 18:31:29 -0600

    Se agregaron los diseños
    Se agregaron los diseños  y se corrigió el código.

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -8,6 +8,13 @@ function TaskForm() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    //Validar que el titulo y la descripción vengan llenos
+    if (!title.trim() || !description.trim()) { // Verificar si el título está vacío o contiene solo espacios en blanco
+      alert("Compos Titulo y Descripción son obligatorios.");
+      return; // Detener el envío del formulario si el título está vacío
+    }
+
     createTask({
       title,
       description,
@@ -17,20 +24,25 @@ function TaskForm() {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        placeholder="Escribe la tarea"
-        onChange={(e) => setTitle(e.target.value)}
-        value={title}
-        autoFocus
-      />
-      <textarea
-        placeholder="Escribe la descripción de la tarea"
-        onChange={(e) => setDescription(e.target.value)}
-        value={description}
-      ></textarea>
-      <button>Guardar</button>
-    </form>
+    <div className="max-w-md mx-auto">
+      <form onSubmit={handleSubmit} className="bg-slate-800 p-10 mb-4">
+        <h1 className="text-2xl font-bold text-white mb-3">Tareas React</h1>
+        <input
+          placeholder="Escribe la tarea"
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}
+          className="bg-slate-300 p-3 w-full mb-2"
+          autoFocus
+        />
+        <textarea
+          placeholder="Escribe la descripción de la tarea"
+          onChange={(e) => setDescription(e.target.value)}
+          value={description}
+          className="bg-slate-300 p-3 w-full mb-2"
+        ></textarea>
+        <button className="bg-indigo-500 p-3 py-1 rounded-md text-white">Guardar</button>
+      </form>
+    </div>
   );
 }
 
